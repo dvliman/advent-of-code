e055c28e32cedb51fc94323d3cb6f6d6fc9e9892
David Liman <limanoit@gmail.com>
2024-12-02 11:14:38 -0800

    day 1

---
+++ src/dvliman/2024/day1.clj
@@ -0,0 +1,32 @@
+(ns dvliman.2024.day1
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn read-lines [file-path]
+  (with-open [reader (io/reader (io/resource file-path))]
+    (doall (line-seq reader))))
+
+(defn parse-int [x]
+  (Integer/parseInt x))
+
+(let [locations (->> "2024/day1.txt"
+                     read-lines
+                     (map #(str/split % #" ")))
+      group1 (sort (map (comp parse-int first) locations))
+      group2 (sort (map (comp parse-int last) locations))]
+  (->>
+   (map vector group1 group2)
+   (map (fn distance' [[x y]] (Math/abs (- y x))))
+   (reduce +)))
+;; => 2031679
+
+(let [locations (->> "2024/day1.txt"
+                     read-lines
+                     (map #(str/split % #" ")))
+      group1 (sort (map (comp parse-int first) locations))
+      freqs (frequencies (map (comp parse-int last) locations))]
+  (->>
+   group1
+   (map (fn similarity' [x] (* x (get freqs x 0))))
+   (reduce +)))
+;; => 19678534
