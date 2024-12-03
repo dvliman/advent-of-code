0daf0a21bfb6fd94f7cc5af3c54b990bd53f4f2b
David Liman <limanoit@gmail.com>
2024-12-03 14:23:09 -0800

    day 03

---
+++ src/dvliman/2024/day03.clj
@@ -0,0 +1,40 @@
+(ns dvliman.2024.day03
+  (:require [clojure.java.io :as io]))
+
+(defn parse-int [x]
+  (Integer/parseInt x))
+
+#_(->> "2024/day3.txt"
+     io/resource
+     io/reader
+     line-seq
+     (apply str)
+     (re-seq #"mul\(([\d]+),([\d]+)\)")
+     (map (juxt (comp parse-int second) (comp parse-int last)))
+     (map (partial apply *))
+     (reduce +))
+;; => 166357705
+
+(->> "2024/day3.txt"
+     io/resource
+     io/reader
+     line-seq
+     (apply str)
+     (re-seq #"(mul\(([\d]+),([\d]+)\)|do\(\)|don\'t\(\))")
+     (reduce (fn [[enabled total] [_ operand left right]]
+
+               (cond
+                 (and enabled (some? left) (some? right))
+                 [enabled (+ total (* (parse-int left) (parse-int right)))]
+
+                 (= operand "do()")
+                 [true total]
+
+                 (= operand "don't()")
+                 [false total]
+
+                 :else
+                 [enabled total])
+               ) [true 0])
+     last)
+;; => 88811886
